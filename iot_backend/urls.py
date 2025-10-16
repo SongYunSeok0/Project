1b46fe70a168a5ecfb49d8cb8dcf72cd2d6b27f2
Ayewww <153438832+Ayewww@users.noreply.github.com>
2025-10-16 16:03:31 +0900

    Delete iot_backend directory

--- iot_backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from api.views import sensor_data, latest_data
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('sensor/', sensor_data),
-    path('latest/', latest_data),
-]
