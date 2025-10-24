0cd619cf11f9301cf5741589d32cb1bedc5191fb
Ayewww <gudwns6518@gmail.com>
2025-10-24 11:14:08 +0900

    app

---
+++ MyRythm/common/src/main/java/com/example/common/MyRythmApplication.kt
@@ -0,0 +1,14 @@
+package com.example.common
+
+import android.app.Application
+
+class myRhythmApplication : Application() {
+    /*override fun onCreate() {
+        super.onCreate()
+        myRhythmApplication = this
+    }
+    companion object{
+        private lateinit var myRhythmApplication: MyRhythmApplication
+        fun getMyRhythmApplication() = myRhythmApplication
+    }*/
+}
