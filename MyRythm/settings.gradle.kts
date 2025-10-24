a3d9ed611c9fa04ab688909bae7c952088f3082a
Ayewww <gudwns6518@gmail.com>
2025-10-24 10:02:05 +0900

    app
    Signed-off-by: Ayewww <gudwns6518@gmail.com>

---
+++ MyRythm/settings.gradle.kts
@@ -0,0 +1,33 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyRythm"
+include(":app")
+include(":feature")
+include(":feature:login")
+include(":feature:main")
+include(":feature:mypage")
+include(":feature:scheduler")
+include(":feature:news")
+include(":feature:map")
+include(":data")
+include(":domain")
+include(":common")
