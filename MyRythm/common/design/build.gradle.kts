beb21e1e93ee1143f8ce4d1fac3bca93c8ce8c7c
Ayewww <gudwns6518@gmail.com>
2025-10-27 11:05:48 +0900

    mypage

--- MyRythm/common/design/build.gradle.kts
+++ MyRythm/common/design/build.gradle.kts
@@ -5,11 +5,12 @@ plugins {
 }
 
 android {
-    namespace = "com.example.feature"
+    namespace = "com.example.common"
     compileSdk = 36
 
     defaultConfig {
         minSdk = 24
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -23,18 +24,15 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
         isCoreLibraryDesugaringEnabled = true
     }
-
     kotlinOptions {
         jvmTarget = "21"
     }
 
-    // ✅ Compose 활성화
     buildFeatures {
         compose = true
     }
@@ -49,27 +47,20 @@ kotlin {
 }
 
 dependencies {
+
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
-    // ✅ AndroidX 기본
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
 
-    // ✅ Compose BOM (버전 통합)
     implementation(platform(libs.androidx.compose.bom))
-
-    // ✅ Compose UI 세트
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.runtime:runtime")
-    implementation("androidx.compose.animation:animation")
-
-    // ✅ 디버그용 툴링
     debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.material:material-icons-extended:1.5.0")
 
-    // ✅ 테스트
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
