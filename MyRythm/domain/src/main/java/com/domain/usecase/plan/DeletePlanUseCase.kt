186ec42ba6d000bf8dc8e4db8e8a23733df14caa
SongYunSeok0 <qkfrus2352@gmail.com>
2025-11-11 15:17:37 +0900

    11-11.15:16

---
+++ MyRythm/domain/src/main/java/com/domain/usecase/plan/DeletePlanUseCase.kt
@@ -0,0 +1,12 @@
+package com.domain.usecase.plan
+
+import com.domain.repository.PlanRepository
+import javax.inject.Inject
+
+class DeletePlanUseCase @Inject constructor(
+    private val repository: PlanRepository
+) {
+    suspend operator fun invoke(userId: String, planId: Long) {
+        repository.delete(userId, planId)
+    }
+}
