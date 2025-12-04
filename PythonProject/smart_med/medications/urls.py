d8bf5b10b88ecc93f566daaa13086ed8b5f17b8a
Song <qkfrus6623@naver.com>
2025-12-04 20:10:49 +0900

    Seok

--- PythonProject/smart_med/medications/urls.py
+++ PythonProject/smart_med/medications/urls.py
@@ -11,14 +11,14 @@ from .views import (
 
 urlpatterns = [
     # RegiHistory
-    path("regihistory/", RegiHistoryListCreateView.as_view(), name="regihistory_list_create"),
-    path("regihistory/<int:pk>/", RegiHistoryUpdateView.as_view(), name="regihistory_update"),
-    path("regihistory/<int:pk>/delete/", RegiHistoryDeleteView.as_view(), name="regihistory_delete"),
+    path("med/regihistory/", RegiHistoryListCreateView.as_view(), name="regihistory_list_create"),
+    path("med/regihistory/<int:pk>/", RegiHistoryUpdateView.as_view(), name="regihistory_update"),
+    path("med/regihistory/<int:pk>/delete/", RegiHistoryDeleteView.as_view(), name="regihistory_delete"),
 
     # Plan
-    path("plan/", PlanListView.as_view(), name="plan_list_create"),
-    path("plan/<int:pk>/", PlanUpdateView.as_view(), name="plan_update"),
-    path("plan/<int:pk>/delete/", PlanDeleteView.as_view(), name="plan_delete"),
+    path("med/plan/", PlanListView.as_view(), name="plan_list_create"),
+    path("med/plan/<int:pk>/", PlanUpdateView.as_view(), name="plan_update"),
+    path("med/plan/<int:pk>/delete/", PlanDeleteView.as_view(), name="plan_delete"),
 
     # Today plans
     # path("plan/today/", TodayPlansView.as_view(), name="today_plans"),
