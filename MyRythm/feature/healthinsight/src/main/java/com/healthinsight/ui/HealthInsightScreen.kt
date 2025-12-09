cfb50da83c3909845109e1cfb2466e26377fa600
yun <yun@sesac.com>
2025-12-09 12:10:54 +0900

    1209 12:08 건강인사이트 스크린 일부 디자인/폰트 적용

--- MyRythm/feature/healthinsight/src/main/java/com/healthinsight/ui/HealthInsightScreen.kt
+++ MyRythm/feature/healthinsight/src/main/java/com/healthinsight/ui/HealthInsightScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -29,7 +28,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -427,7 +425,7 @@ private fun MedicationChart(
                 Text(
                     text = ontimeRateText,
                     style = MaterialTheme.typography.labelSmall,
-                    color = Color.Gray
+                    color =  MaterialTheme.colorScheme.surfaceVariant
                 )
                 Text(
                     text = "$onTimeRate%",
