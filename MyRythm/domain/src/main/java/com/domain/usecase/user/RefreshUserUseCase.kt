0f8b091ca74faf4f65e8bdd4b1b4c6cdaf8bf9bf
Ayewww <gudwns6518@gmail.com>
2026-01-06 12:43:26 +0900

    result랑 apiresult 문제

--- MyRythm/domain/src/main/java/com/domain/usecase/user/RefreshUserUseCase.kt
+++ MyRythm/domain/src/main/java/com/domain/usecase/user/RefreshUserUseCase.kt
@@ -1,5 +1,7 @@
 package com.domain.usecase.user
 
+import com.domain.model.ApiResult
+import com.domain.model.DomainError
 import com.domain.model.User
 import com.domain.repository.UserRepository
 import javax.inject.Inject
@@ -7,5 +9,10 @@ import javax.inject.Inject
 class RefreshUserUseCase @Inject constructor(
     private val repo: UserRepository
 ) {
-    suspend operator fun invoke(userId: String): User = repo.syncUser()
+    suspend operator fun invoke(userId: String): ApiResult<User> {
+        return repo.syncUser().fold(
+            onSuccess = { ApiResult.Success(it) },
+            onFailure = { ApiResult.Failure(DomainError.Unknown(it.message)) }
+        )
+    }
 }
--- MyRythm/domain/src/main/java/com/domain/usecase/user/UpdateUserUseCase.kt
+++ MyRythm/domain/src/main/java/com/domain/usecase/user/UpdateUserUseCase.kt
@@ -1,5 +1,7 @@
 package com.domain.usecase.user
 
+import com.domain.model.ApiResult
+import com.domain.model.DomainError
 import com.domain.model.User
 import com.domain.repository.UserRepository
 import javax.inject.Inject
@@ -7,5 +9,14 @@ import javax.inject.Inject
 class UpdateUserUseCase @Inject constructor(
     private val repo: UserRepository
 ) {
-    suspend operator fun invoke(user: User): Boolean = repo.updateUser(user)
+    suspend operator fun invoke(user: User): ApiResult<Unit> {
+        return repo.updateUser(user).fold(
+            onSuccess = { ApiResult.Success(it) },
+            onFailure = {
+                ApiResult.Failure(
+                    DomainError.Unknown(it.message)
+                )
+            }
+        )
+    }
 }
